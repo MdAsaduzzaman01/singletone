95d9703c2e17e576b7194a7acc0f8381973ea450
Md Asaduzzaman <asaduzzaman@cslsoft.com>
2022-04-27 09:42:21 +0600

    Add project files.

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(FromTeacher,FromStudent);
+            Console.ReadLine();
+        }
+        private static void FromTeacher()
+        {
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From Teacher");
+
+        }
+        private static void FromStudent()
+        {
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo
+{
+   public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        private static readonly object instancelock=new object(); 
+        public static Singleton GetInstance
+        {
+            get
+            {
+                //Double Check for time safety
+                if (instance == null)
+                {
+                    //Lock for Multi-threading
+                    lock (instancelock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+
+                        }
+                       
+                    }
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Instance Created " + counter + " times");
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
